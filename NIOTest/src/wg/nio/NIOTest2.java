f232c1237f6f640a650029e9257cf3f689a6250c
wangge <Administrator@USER-20171110ZA>
2019-01-07 00:55:56 +0800

    测试图片

--- NIOTest/src/wg/nio/NIOTest2.java
+++ NIOTest/src/wg/nio/NIOTest2.java
@@ -1,5 +1,10 @@
 package wg.nio;
 
-public class NIOTest2 {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+public class NIOTest2 {
+	public static void main(String[] args) throws FileNotFoundException {
+		FileInputStream fis1=new FileInputStream("timg.jpg");
+	}
 }
